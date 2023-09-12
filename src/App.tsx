9ff83923e584b85b6abc0409b30d394d511af526
Richi <richipineda2@gmail.com>
2023-09-12 13:14:26 +0100

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Timer from './components/Timer';
 
 function App() {
   return (
-    <div className="flex flex-col items-center h-screen overflow-hidden bg-pale-silver">
+    <div className="flex flex-col items-center justify-center h-screen overflow-hidden bg-gray-100">
       <Timer />
     </div>
   );
